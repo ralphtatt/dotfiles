646de47a2fb6338f0b3198e178d33d54e7696592
RJ <ralphtatt@gmail.com>
2026-01-15 21:38:37 +0000

    Basic neovim set up with Lazy and Mason

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,22 +1,4 @@
--- Basic settings
-vim.opt.number = true              -- Show line numbers
-vim.opt.relativenumber = true      -- Relative line numbers
-vim.opt.expandtab = true           -- Use spaces instead of tabs
-vim.opt.tabstop = 2                -- 2 spaces per tab
-vim.opt.shiftwidth = 2             -- 2 spaces for indentation
-vim.opt.softtabstop = 2            -- 2 spaces in insert mode
-vim.opt.smartindent = true         -- Smart indentation
-vim.opt.wrap = false               -- Don't wrap long lines
-vim.opt.ignorecase = true          -- Case insensitive search
-vim.opt.smartcase = true           -- Unless uppercase in search
-vim.opt.termguicolors = true       -- True color support
-vim.opt.cursorline = true          -- Highlight current line
-vim.opt.splitbelow = true          -- New horizontal split below
-vim.opt.splitright = true          -- New vertical split right
-vim.opt.clipboard = "unnamedplus"  -- Use system clipboard
-
--- Key mappings
-vim.g.mapleader = " "
-
--- Clear search highlights
-vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", { noremap = true, silent = true })
+-- ~/.config/nvim/init.lua
+require("core.options")
+require("core.keymaps")
+require("core.lazy")
